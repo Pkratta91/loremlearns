3246b11f09366efea27e02b9eeb1662216015ca1
Pkratta91 <65401115+Pkratta91@users.noreply.github.com>
2020-05-25 09:05:04 +0100

    first commit

---
+++ assets/javascript.js
@@ -0,0 +1,8 @@
+function dropdownmenuFunction() {
+  var x = document.getElementById("menuLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
